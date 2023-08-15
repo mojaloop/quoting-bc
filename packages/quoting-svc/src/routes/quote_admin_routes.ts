a19d9d81bf3587d9499ecf1cf6b7a1a7a9da4f1c
SithuKyaw-Thitsaworks <Sithu.Kyaw@thitsaworks.com>
2023-08-16 01:21:19 +0630

    Decode ILP packet data to show in Quote Detail UI

--- packages/quoting-svc/src/routes/quote_admin_routes.ts
+++ packages/quoting-svc/src/routes/quote_admin_routes.ts
@@ -38,169 +38,193 @@
  --------------
  **/
 
-"use strict";
+ "use strict";
 
-import express from "express";
-import { ILogger } from "@mojaloop/logging-bc-public-types-lib";
-import { IBulkQuoteRepo, IQuoteRepo } from "@mojaloop/quoting-bc-domain-lib";
-import { check } from "express-validator";
-import { BaseRoutes } from "./base/base_routes";
-
-export class QuotingAdminExpressRoutes extends BaseRoutes {
-    constructor(
-        quotesRepo: IQuoteRepo,
-        bulkQuoteRepo: IBulkQuoteRepo,
-        logger: ILogger
-    ) {
-        super(logger, quotesRepo, bulkQuoteRepo);
-        this.logger.createChild(this.constructor.name);
-
-        this.mainRouter.get("/quotes", this.getAllQuotes.bind(this));
-
-        this.mainRouter.get("/bulk-quotes", this.getAllBulkQuotes.bind(this));
-
-        this.mainRouter.get(
-            "/quotes/:id",
-            [
-                check("id")
-                    .isString()
-                    .notEmpty()
-                    .withMessage("id must be a non empty string"),
-            ],
-            this.getQuoteById.bind(this)
-        );
-
-        this.mainRouter.get(
-            "/bulk-quotes/:id",
-            [
-                check("id")
-                    .isString()
-                    .notEmpty()
-                    .withMessage("id must be a non empty string"),
-            ],
-            this.getBulkQuoteById.bind(this)
-        );
-    }
-
-    private async getAllQuotes(
-        _req: express.Request,
-        res: express.Response,
-        _next: express.NextFunction
-    ) {
-        const transactionId = _req.query.transactionId as string;
-        const quoteId = _req.query.quoteId as string;
-        const amountType = _req.query.amountType as string;
-        const transactionType = _req.query.transactionType as string;
-
-        try {
-            let fetched;
-
-            if (quoteId || transactionId || amountType || transactionType) {
-                this.logger.info("Filtering quotes");
-                fetched = await this.quoteRepo.searchQuotes(
-                    transactionId,
-                    quoteId,
-                    amountType,
-                    transactionType
-                );
-            } else {
-                this.logger.info("Fetching all quotes");
-                fetched = await this.quoteRepo.getQuotes();
-            }
-
-            res.send(fetched);
-        } catch (err: unknown) {
-            this.logger.error(err);
-            res.status(500).json({
-                status: "error",
-                msg: (err as Error).message,
-            });
-        }
-    }
-
-    private async getAllBulkQuotes(
-        _req: express.Request,
-        res: express.Response,
-        _next: express.NextFunction
-    ) {
-        this.logger.info("Fetching all bulk quotes");
-        try {
-            const fetched = await this.bulkQuoteRepo.getBulkQuotes();
-            res.send(fetched);
-        } catch (err: unknown) {
-            this.logger.error(err);
-            res.status(500).json({
-                status: "error",
-                msg: (err as Error).message,
-            });
-        }
-    }
-
-    private async getQuoteById(
-        req: express.Request,
-        res: express.Response,
-        _next: express.NextFunction
-    ) {
-        if (!this.validateRequest(req, res)) {
-            return;
-        }
-
-        const id = req.params["id"] ?? null;
-        this.logger.info(`Fetching Oracle [${id}].`);
-
-        this.logger.info("Fetching quote by id " + id);
-
-        try {
-            const fetched = await this.quoteRepo.getQuoteById(id);
-            if (!fetched) {
-                res.status(404).json({
-                    status: "error",
-                    msg: "Quote not found",
-                });
-
-                return;
-            }
-            res.send(fetched);
-        } catch (err: unknown) {
-            this.logger.error(err);
-            res.status(500).json({
-                status: "error",
-                msg: (err as Error).message,
-            });
-        }
-    }
-
-    private async getBulkQuoteById(
-        req: express.Request,
-        res: express.Response,
-        _next: express.NextFunction
-    ) {
-        if (!this.validateRequest(req, res)) {
-            return;
-        }
-
-        const id = req.params["id"] ?? null;
-        this.logger.info(`Fetching Oracle [${id}].`);
-
-        this.logger.info("Fetching bulk quote by id " + id);
-
-        try {
-            const fetched = await this.bulkQuoteRepo.getBulkQuoteById(id);
-            if (!fetched) {
-                res.status(404).json({
-                    status: "error",
-                    msg: "Bulk Quote not found",
-                });
-
-                return;
-            }
-            res.send(fetched);
-        } catch (err: unknown) {
-            this.logger.error(err);
-            res.status(500).json({
-                status: "error",
-                msg: (err as Error).message,
-            });
-        }
-    }
-}
+ import express from "express";
+ import { ILogger } from "@mojaloop/logging-bc-public-types-lib";
+ import { IBulkQuoteRepo, IQuoteRepo } from "@mojaloop/quoting-bc-domain-lib";
+ import { check } from "express-validator";
+ import { BaseRoutes } from "./base/base_routes";
+ import * as IlpPacket from 'ilp-packet';
+ 
+ export class QuotingAdminExpressRoutes extends BaseRoutes {
+     constructor(
+         quotesRepo: IQuoteRepo,
+         bulkQuoteRepo: IBulkQuoteRepo,
+         logger: ILogger
+     ) {
+         super(logger, quotesRepo, bulkQuoteRepo);
+         this.logger.createChild(this.constructor.name);
+ 
+         this.mainRouter.get("/quotes", this.getAllQuotes.bind(this));
+ 
+         this.mainRouter.get("/bulk-quotes", this.getAllBulkQuotes.bind(this));
+ 
+         this.mainRouter.get(
+             "/quotes/:id",
+             [
+                 check("id")
+                     .isString()
+                     .notEmpty()
+                     .withMessage("id must be a non empty string"),
+             ],
+             this.getQuoteById.bind(this)
+         );
+ 
+         this.mainRouter.get(
+             "/bulk-quotes/:id",
+             [
+                 check("id")
+                     .isString()
+                     .notEmpty()
+                     .withMessage("id must be a non empty string"),
+             ],
+             this.getBulkQuoteById.bind(this)
+         );
+     }
+ 
+     private async getAllQuotes(
+         _req: express.Request,
+         res: express.Response,
+         _next: express.NextFunction
+     ) {
+         const transactionId = _req.query.transactionId as string;
+         const quoteId = _req.query.quoteId as string;
+         const amountType = _req.query.amountType as string;
+         const transactionType = _req.query.transactionType as string;
+ 
+         try {
+             let fetched;
+ 
+             if (quoteId || transactionId || amountType || transactionType) {
+                 this.logger.info("Filtering quotes");
+                 fetched = await this.quoteRepo.searchQuotes(
+                     transactionId,
+                     quoteId,
+                     amountType,
+                     transactionType
+                 );
+             } else {
+                 this.logger.info("Fetching all quotes");
+                 fetched = await this.quoteRepo.getQuotes();
+             }
+ 
+             res.send(fetched);
+         } catch (err: unknown) {
+             this.logger.error(err);
+             res.status(500).json({
+                 status: "error",
+                 msg: (err as Error).message,
+             });
+         }
+     }
+ 
+     private async getAllBulkQuotes(
+         _req: express.Request,
+         res: express.Response,
+         _next: express.NextFunction
+     ) {
+         this.logger.info("Fetching all bulk quotes");
+         try {
+             const fetched = await this.bulkQuoteRepo.getBulkQuotes();
+             res.send(fetched);
+         } catch (err: unknown) {
+             this.logger.error(err);
+             res.status(500).json({
+                 status: "error",
+                 msg: (err as Error).message,
+             });
+         }
+     }
+ 
+ 
+     private async getQuoteById(
+         req: express.Request,
+         res: express.Response,
+         _next: express.NextFunction
+     ) {
+         if (!this.validateRequest(req, res)) {
+             return;
+         }
+ 
+         const id = req.params["id"] ?? null;
+         this.logger.info(`Fetching Oracle [${id}].`);
+ 
+         this.logger.info("Fetching quote by id " + id);
+ 
+         try {
+             const fetched = await this.quoteRepo.getQuoteById(id);
+ 
+             if (!fetched) {
+                 res.status(404).json({
+                     status: "error",
+                     msg: "Quote not found",
+                 });
+ 
+                 return;
+             }
+             else {
+ 
+                 if (fetched.ilpPacket) {
+                     let ilpPacketBuffer: any = Buffer.from(fetched.ilpPacket, 'base64');
+                     let decodedIlpPacket: any;
+ 
+                     try {
+                         decodedIlpPacket = IlpPacket.deserializeIlpPacket(ilpPacketBuffer);
+                         const decodedIlpPacketData = JSON.parse(Buffer.from(decodedIlpPacket.data.data.toString('utf8'), 'base64').toString('utf8'));
+                         fetched.ilpPacket = JSON.stringify(decodedIlpPacketData, null, 2);
+ 
+                         //console.log(decodedIlpPacketData)
+                     }
+                     catch (error) {
+                         this.logger.error(`Cannot deserialize IlpPacket: ${error}`);
+                     }
+                 }
+             }
+ 
+             res.send(fetched);
+ 
+         } catch (err: unknown) {
+             this.logger.error(err);
+             res.status(500).json({
+                 status: "error",
+                 msg: (err as Error).message,
+             });
+         }
+     }
+ 
+     private async getBulkQuoteById(
+         req: express.Request,
+         res: express.Response,
+         _next: express.NextFunction
+     ) {
+         if (!this.validateRequest(req, res)) {
+             return;
+         }
+ 
+         const id = req.params["id"] ?? null;
+         this.logger.info(`Fetching Oracle [${id}].`);
+ 
+         this.logger.info("Fetching bulk quote by id " + id);
+ 
+         try {
+             const fetched = await this.bulkQuoteRepo.getBulkQuoteById(id);
+             if (!fetched) {
+                 res.status(404).json({
+                     status: "error",
+                     msg: "Bulk Quote not found",
+                 });
+ 
+                 return;
+             }
+             res.send(fetched);
+         } catch (err: unknown) {
+             this.logger.error(err);
+             res.status(500).json({
+                 status: "error",
+                 msg: (err as Error).message,
+             });
+         }
+     }
+ }
+ 
